904b2b3c9532e37e0b72680956286b38c30f6517
Rajeshd01 <rajeshd01042004@gmail.com>
2024-07-18 16:36:49 +0530

    implemented login and signup form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div></div>;
+  const [rememberLogin, setRememberLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
+
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden absolute sm:block w-full h-full object-cover"
+          src="/src/assets/img/background1.jpg"
+          alt=""
+        />
+        <div className="bg-black/70 fixed top-0 left-0 w-full h-screen" />
+
+        <div className="fixed w-full px-4 py-24 z-20">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/80 rounded-lg">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-nsans-bold justify-center content-center">
+                Login
+              </h1>
+
+              <form
+                onSubmit={handleFormSubmit}
+                className="w-full flex flex-col py-4"
+              >
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded-lg"
+                  placeholder="email"
+                  type="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded-lg"
+                  placeholder="password"
+                  type="password"
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button className="bg-red-600 py-3 my-3 rounded-lg font-nsans-bold">
+                  SignIn
+                </button>
+
+                <div className="flex justify-between items-center text-gray-600">
+                  <p>
+                    <input
+                      type="checkbox"
+                      className="mr-2"
+                      checked={rememberLogin}
+                      onChange={(e) => setRememberLogin(!rememberLogin)}
+                    />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="my-4">
+                  <span className="text-gray-600 mr-2">New to netflix?</span>
+                  <Link to="/src/pages/Signup.jsx">Signup</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
