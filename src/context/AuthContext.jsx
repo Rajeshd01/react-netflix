113a584ab6f10b34418e24f9b88832412f46cee4
Rajeshd01 <rajeshd01042004@gmail.com>
2024-07-19 21:58:14 +0530

    fixed firebases issue and signup success

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import { auth } from "../services/firebase"; // Assuming 'db' isn't used
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  // Corrected 'childeren' to 'children'
+  const [user, setUser] = useState(null); // Set initial user to null
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password); // Ensure it returns a promise
+  }
+
+  function logIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logOut() {
+    return signOut(auth);
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, signUp, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function UserAuth() {
+  return useContext(AuthContext);
+}
